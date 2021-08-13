baa8abc71cf44f44cdd05a9c66269ffd0a832243
Semakov Andrey <semakov.andrey.web@yandex.ru>
2021-08-13 09:03:14 +0400

    ts

--- src/store/repos/tracking.repo.ts
+++ src/store/repos/tracking.repo.ts
@@ -14,13 +14,24 @@ import type { ITimeTrack } from 'entities/TimeTrack';
 import type { ITrack } from 'entities/Track';
 import type { IStates } from 'store/states';
 
-type ParamsState = Array<ITrack> & Nullable<ITrack> & Nullable<ITimeTrack> & boolean;
+type ParamsState = Array<ITrack> | Nullable<ITrack> | Nullable<ITimeTrack> | boolean;
 type ParamsActions = ITrack & Nullable<ITimeTrack> & boolean;
 
 type TMapStates = Record<string, ParamsState>;
 type TMapActions = Record<string, (...args: Array<ParamsActions>) => Action>;
 
-const mapStates = ({ tracking }: IStates): TMapStates => tracking as unknown as TMapStates;
+const mapStates = ({ tracking: {
+  tracks,
+  currentTrack,
+  currentTimeTrack,
+  inTracking
+} }: IStates): TMapStates => ({
+  tracks,
+  currentTrack,
+  currentTimeTrack,
+  inTracking
+});
+
 const mapActions: TMapActions = {
   addNewTrack,
   setCurrentTrack,
