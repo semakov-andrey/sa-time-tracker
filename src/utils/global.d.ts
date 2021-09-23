9e6b91cb635cd10bdb8efd2e836a5b8b36ee36ab
Semakov Andrey <semakov.andrey.web@yandex.ru>
2021-09-24 00:15:15 +0400

    Types

--- src/utils/global.d.ts
+++ src/utils/global.d.ts
@@ -1,17 +1,39 @@
+type FunctionType<T = unknown, R = unknown> = (...args: Array<T>) => R;
+
 type ObjectKey = string | number | symbol;
+
+type ObjectType<T = unknown> = Record<ObjectKey, T>;
+
+type ObjectDefType<K extends string | number | symbol, T = unknown> = Record<K, T>;
 
 type Nullable<T> = T | null;
 
 type Optional<T> = T | undefined;
 
-type EntryOf<T> = { [K in keyof T]: [K, T[K]] }[keyof T];
+type ResolveType = (value: unknown) => void;
+
+type RejectType = (reason?: unknown) => void;
+
+type ArrayInnerType<T> = T extends Array<infer R> ? R : never;
+
+type KeyOf<T> = keyof T;
+
+type ValueOf<T> = T[KeyOf<T>];
+
+type EntryOf<T> = { [K in KeyOf<T>]: [K, T[K]] }[KeyOf<T>];
 
 interface ObjectConstructor {
-  keys<T>(o: T): Array<keyof T>;
+  keys<T>(o: T): Array<KeyOf<T>>;
 
-  values<T>(o: T): Array<T[keyof T]>;
+  values<T>(o: T): Array<T[KeyOf<T>]>;
 
   entries<T>(o: T): Array<EntryOf<T>>;
 
   fromEntries<T>(entries: Array<EntryOf<T>>): T;
 }
+
+interface Array<T> {
+  includes<U>(searchElement: U, fromIndex?: number): U extends T ? boolean : false;
+}
+
+type Modify<T, R> = Omit<T, KeyOf<R>> & R;
--- src/utils/guards.ts
+++ src/utils/guards.ts
@@ -2,8 +2,13 @@ export const isset = <T>(u?: T): u is T => typeof u !== 'undefined';
 
 export const iswritten = <T>(u: T | null): u is T => u !== null;
 
+export const isexists = <T>(u?: T | null): u is T => isset(u) && iswritten(u);
+
 export const isTypeObject = (u: unknown): u is Record<ObjectKey, unknown> =>
   typeof u === 'object' && u !== null && !Array.isArray(u);
+
+export const isKeyOfObject = <T>(u: T, key: ObjectKey): key is keyof T =>
+  isTypeObject(u) && Object.prototype.hasOwnProperty.call(u, key);
 
 type TValueConstructors = StringConstructor | NumberConstructor | BooleanConstructor | ObjectConstructor;
 
@@ -12,9 +17,6 @@ type o<I extends TValueConstructors, T, E extends ObjectKey> = {
 } & {
   [s in E]: InstanceType<I>;
 };
-
-export const isKeyOfObject = <T>(obj: T, key: ObjectKey): key is keyof T =>
-  typeof obj === 'object' && obj !== null && Object.prototype.hasOwnProperty.call(obj, key);
 
 export const checkPropType = <
   I extends TValueConstructors,
@@ -26,3 +28,11 @@ export const checkPropType = <
   key: S
 ): obj is o<I, T, S> =>
   isKeyOfObject(obj, key) && typeof obj[key] === instance.name.toLowerCase();
+
+export const isTypeNumber = (u: unknown): u is number =>
+  typeof u === 'number';
+
+export const isTypeString = (u: unknown): u is string =>
+  typeof u === 'string';
+
+export const isEmptyString = (u: string): boolean => u !== '';
